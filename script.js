fba5d5da7995aa5e7151346c4cccbdbc1b29fd60
jhonatan145 <jhonnygg1208@gmail.com>
2026-06-11 13:41:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-// FAVORITOS
-
-const botones =
-document.querySelectorAll(".favoritoBtn");
-
-botones.forEach(boton => {
-
-    boton.addEventListener("click", () => {
-
-        const stand =
-        boton.dataset.stand;
-
-        let favoritos =
-        JSON.parse(
-        localStorage.getItem("favoritos")
-        ) || [];
-
-        if(!favoritos.includes(stand)){
-
-            favoritos.push(stand);
-
-            localStorage.setItem(
-                "favoritos",
-                JSON.stringify(favoritos)
-            );
-
-            alert(
-            stand + " añadido a favoritos ⭐"
-            );
-
-        }
-
-    });
-
-});
-
 // BUSCADOR
 
 const buscador =
